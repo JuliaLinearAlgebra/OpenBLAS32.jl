17233133237ba823940a1bde798feca7137e7f02
Documenter.jl <documenter@juliadocs.github.io>
2023-07-14 23:47:48 +0000

    build based on 44c2819

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OpenBLAS32","category":"page"},{"location":"#OpenBLAS32","page":"Home","title":"OpenBLAS32","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OpenBLAS32.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OpenBLAS32]","category":"page"}]
+}
